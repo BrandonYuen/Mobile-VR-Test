e12ff32298966ea8efd6dac5ffaa6c4dcc06fa1f
Brandon Yuen <1223259+BrandonYuen@users.noreply.github.com>
2019-05-25 16:16:24 +0200

    Initial

---
+++ Assets/MyData/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 3.5f;
+    public int distanceOfRaycast;
+
+    private float gravity = 10f;
+    private RaycastHit _hit;
+
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRaycast)) {
+            if (Input.GetButtonDown("Fire1") && _hit.transform.CompareTag("Cube")) {
+                _hit.transform.gameObject.GetComponent<Rotate>().changeSpin();
+            }
+        }
+        playerMovement();
+    }
+
+    void playerMovement() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * speed;
+        velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y -= gravity;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
